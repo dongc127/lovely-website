9a112191e3344fa572424f8811670b48d09768aa
Claire Dong <dongc127@newschool.edu>
2020-02-28 15:45:46 -0500

    homework for Agnes

---
+++ core-lab/homework/02/main.js
@@ -0,0 +1,30 @@
+function changeHeading(color,word){
+	document.body.style.backgroundColor=color; 
+	
+}
+
+function  changeFont(word){
+	document.body.style.fontFamily= "Helvetica,Times New Roman";
+
+}
+
+function changeBlockFont(word){
+	document.body.style.fontFamily= "Impact";
+
+}
+// function changeBlockFont(word){
+// 	document.body.style.fontFamily= "Times New Roman";
+
+// }
+
+// function changeFontColor (color,word){
+// 	document.body.style.fontColor=color;
+
+// }
+
+// function changeColor(color)
+
+function changeFontColor2 (word,color){
+ document.body.style.fontColor="Indigo";
+}
+
