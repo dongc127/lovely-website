c0cb9a5f28c53348cfb0e76ea59a5999365f4e94
Claire Dong <dongc127@newschool.edu>
2020-03-06 15:29:00 -0500

    jquery hw and documentation upload

---
+++ core-lab/classwork/06/main.js
@@ -0,0 +1,41 @@
+// function changeHeading(color){
+// 	document.getElementById('heading').style.backgroundColor=color;
+// }
+
+// function changeHeading (color) {
+// 	$('#heading').css({"background-color":color})
+// }
+
+// $("p").css({"background-color": "pink"}); 
+
+// $(".green").css({"background-color": "limegreen"});
+
+
+//function hideParagraphs () {
+//	$("p").hide()
+//}
+
+//function showParagraphs () {
+//	$("p").show()
+//}
+
+// function toggleParagraphs () {
+// 	$("p").toggle();
+// }
+
+// $("p").click(function(){
+//   $("p").css({"background-color": "pink"});
+// });
+
+// function doSomething(thing){
+// 	console.log('hey',thing)
+// }
+
+// $("p").click(function(){
+//   $(this).css({"background-color": "pink"});
+// });
+
+$("p").mouseover(function()){
+	$(this).css{"background-color"
+	}
+}
---
+++ core-lab/homework/03/homework.js
@@ -0,0 +1,8 @@
+console.log('peepeepoopoo');
+
+
+function changeHeading () {
+	$('heading').css({"background-color": "yellow"})
+};
+
+$('p').css({"font-family":"comic sans 24px sans-serif"});
